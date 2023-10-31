2742f1dc0571802d1cc68c33fb293caa792d0084
LucasCyw <131354873+LucasCyw@users.noreply.github.com>
2023-10-31 14:20:47 +0800

    Update MultiplicationTable.test.ts

--- test/MultiplicationTable.test.ts
+++ test/MultiplicationTable.test.ts
@@ -8,9 +8,9 @@ describe('Multiplication Table', () => {
     const end = 1
 
     //when
-    const rendered = table.render(start, end)
+    const rendered = table.create(start, end)
 
     //then
-    expect(rendered).toBe('1*1=1')
+    expect(rendered).toBe('1 * 1 = 1')
   })
 })
