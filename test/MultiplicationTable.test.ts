b546c5da0b7599e98d6f61d8225c9a2a2c2d8c7b
LucasCyw <131354873+LucasCyw@users.noreply.github.com>
2023-10-31 14:26:08 +0800

    Update MultiplicationTable.test.ts

--- test/MultiplicationTable.test.ts
+++ test/MultiplicationTable.test.ts
@@ -12,5 +12,15 @@ describe('Multiplication Table', () => {
 
     //then
     expect(rendered).toBe('1 * 1 = 1')
-  })
-})
+  });
+  it('should return null for invalid inputs', () => {
+    // given
+    const table = new MultiplicationTable()
+    const start = 4;
+    const end = 2;
+    // when 
+    const rendered = table.create(start, end);
+    // then
+    expect(rendered).toBeNull();
+  });
+});
